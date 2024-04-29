ec3dabd05f4ccdc0156e9203092115f6b4977312
MohAsfour97 <mfasfur@gmail.com>
2024-04-29 03:12:47 +0300

    d

---
+++ src/content_option.js
@@ -0,0 +1,185 @@
+const logotext = "Mohammed Asfour";
+const meta = {
+    title: "Mohammed Asfour",
+    description: "I’m Mohammed F Asfour Software engineer _ Full stack devloper,currently working in Riyadh, Saudi Arabia",
+};
+
+const introdata = {
+    title: "I’m Mohammed F Asfour",
+    animated: {
+        first: "I blend Innovation & Software.",
+        second: "I code, design modern websites.",
+        third: "I develop mobile apps too!",
+    },
+    description: "Passionate software engineer turning ideas into elegant code, and crafting solutions to complex problems.. Skilled in Software/Web development. Always coding, always learning.",
+    your_img_url: "https://images.unsplash.com/photo-1713892121959-7179fdca18fc?q=80&w=1936&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+};
+
+const dataabout = {
+    title: "A glimpse about myself",
+    aboutme: "Experienced Software Engineer skilled in designing, constructing, and testing software systems, with a track record of impactful contributions in the tech sector. B.Sc. in Software Engineering from Prince Sultan University College of CS & IS. Currently looking to utilize my expertise and understanding of all aspects of the software development life cycle (SDLC) to continue professional development.",
+};
+const worktimeline = [{
+        jobtitle: "Software Engineer COOP",
+        where: "WSM Co.",
+        date: "2022",
+    },
+    {
+        jobtitle: "Front End developer, UI/UX Designer",
+        where: "RIOTU labs",
+        date: "2023",
+    },
+    {
+        jobtitle: "Web developer",
+        where: "Pencil Agency",
+        date: "2024",
+    },
+];
+
+const skills = [{
+        name: "Java",
+        value: 90,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "HTML",
+        value: 90,
+    },
+    {
+        name: "React",
+        value: 85,
+    },
+    {
+        name: "CSS",
+        value: 90,
+    },
+    {
+        name: "TailwindCSS",
+        value: 85,
+    },
+    
+    {
+        name: "Wordpress",
+        value: 85,
+    },
+    {
+        name: "Figma UI",
+        value: 85,
+    },
+    {
+        name: "Jira",
+        value: 95,
+    },
+    {
+        name: "JUnit",
+        value: 65,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "Designing responsive & modern User Interfaces using tools such as Figma to achieve seamless overall User Experience, using my HCI Skills",
+    },
+    {
+        title: "Web Development",
+        description: "Choosing appropriate Technology stacks and the best WebFrameworks and methadologies such as React JS, HTML5, Tailwind CSS, JavaScript, Strapi, and WordPress. Crafting dynamic UIs, semantic content, and responsive designs for seamless user experiences. Such Example is this portfolio Website is developed and designed by me using React with Bootstrap stack" ,
+    },
+    {
+        title: "Software Testing",
+        description: "Experienced in implementing comprehensive testing strategies and Quality Assurance using JUnit and automation testing, Black/White box approach test cases.",
+    },
+    {
+        title: "Data Gathering & Analysis",
+        description: "Skilled in gathering and analyzing data using data science techniques, extracting valuable insights to drive informed decision-making.",
+    },
+    {
+        title: "Software Business Analysis & Documentation",
+        description: "Fluently Identify and analyse the existing business challenges and turn them into reliable and effective Consultations, solutions,requirments,documents , and plans",
+    },
+    {
+        title: "Object-Oriented-Programming(OOP)",
+        description: "Java developer adept in OOP principles, creating robust and scalable applications. Experienced with Spring and Hibernate frameworks for efficient backend development.",
+    },
+    {
+        title: "AI Integration & LLMs",
+        description: "AI Integration Specialist skilled in seamlessly integrating AI technologies into websites and systems. Proficient in leveraging OpenAI playgrounds and autotuning Large Language Models (LLMs) for enhanced functionality. Experienced in designing and implementing AI-driven solutions, optimizing user experiences with cutting-edge technology.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://images.unsplash.com/photo-1713900233557-51aaaca77d31?q=80&w=400&h=400&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3Dhttps://images.unsplash.com/photo-1713900462062-d421d62def56?q=80&w=1843&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D/400/800/?grayscale",
+        description: "PAS ERP Responsive Website",
+        link: "https://pas.sa/en/home/",
+    },
+
+    {
+        img: "https://images.unsplash.com/photo-1714286552957-27de7764b597?q=80&w=400&h=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        description: "Mastering ChatGPT Certificate.",
+        link: "https://drive.google.com/file/d/1EQUEuG-3weKlrJKdMAQVhCon5qgfj5Yx/view",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "Current Enrolled Courses & Updates",
+        link: "#",
+    },
+    
+    
+    {
+        img: "https://images.unsplash.com/photo-1714286314601-10e4d4189b76?q=80&w=400&h=500&auto=format&fit=crop",
+        description: "PSU GPT AI Project",
+        link: "https://psugpt.ai/",
+    },
+
+
+    {
+        img: "https://images.unsplash.com/photo-1714286264849-aca448aa396b?q=80&w=400&h=600&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        description: "Dono PSU Graduation Project",
+        link: "https://donoapp.com/",
+    },
+
+    
+    {
+        img: "https://images.unsplash.com/photo-1714286618785-fba8333312fa?q=80&w=300&h=300&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        description: "Masar SBF Internal Portal Project",
+        link: "https://sportsboulevard.sa/en",
+    },
+   
+   
+    {
+        img: "https://images.unsplash.com/photo-1714295074011-b387cb084b06?q=80&w=400&h=700&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        description: "MY CV",
+        link: "https://drive.google.com/file/d/1nmkxUtyRVROZ-DACh52y7Mk9CCi2Ojn4/view?usp=sharing",
+    },
+   
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "mfasfur@gmail.com",
+    YOUR_FONE: "(+966)-595254091",
+    description: "Tanduf st. , Al Ghadir, 4028 , Riyadh, Saudi Arabia",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/MohAsfour97",
+    linkedin: "https://www.linkedin.com/in/mohammed-asfour-prof1997/",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
